19630776ec1a2a966ab63a0882063c1bb5287257
lilia <liliakai@gmail.com>
2018-01-16 14:33:06 +0100

    Parse some data out of the current tab

--- popup.js
+++ popup.js
@@ -1,8 +1,21 @@
+var data = null;
+
+// Parse the current page
+chrome.tabs.executeScript(tabId, {
+  code: 'document.querySelector(' + JSON.stringify('body') + ')'
+}, function(results) {
+  if (results[0]) {
+      data = results[0]
+  } else {
+      chrome.pageAction.hide(tabId);
+  }
+});
+
 document.forms["form"].addEventListener('submit', function(event) {
   event.preventDefault();
   fetch('http://localhost:8000', {
     method : 'POST',
-    body   : 'email',
+    body   : data,
   }).then(function(response) {
     console.log(response.status);
   }).catch(function(error) {
