fab6385de928bd815c9b60c55650fd169540a114
lilia <liliakai@gmail.com>
2018-03-12 18:58:12 +0100

    Add auth token setting

--- options.js
+++ options.js
@@ -1,10 +1,16 @@
 var serverUrl = localStorage.getItem('serverUrl');
 $('#serverUrl').val(serverUrl);
+var authToken = localStorage.getItem('authToken');
+$('#authToken').val(authToken);
 
 $('#form').submit(function(event) {
   event.preventDefault();
   var serverUrl = $('#serverUrl').val();
   if (typeof serverUrl === 'string' && serverUrl.length > 0) {
     localStorage.setItem('serverUrl', serverUrl);
+  }
+  var authToken = $('#authToken').val();
+  if (typeof authToken === 'string' && authToken.length > 0) {
+    localStorage.setItem('authToken', authToken);
   }
 });
