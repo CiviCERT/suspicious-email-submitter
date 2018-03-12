b54dda8a49c1396ecc8266ab8ab7c89fb906ab6f
lilia <liliakai@gmail.com>
2018-03-12 18:58:32 +0100

    Submit emails to MISP

--- popup.js
+++ popup.js
@@ -14,14 +14,37 @@ if (data) {
   document.forms["form"].addEventListener('submit', function(event) {
     event.preventDefault();
     var serverUrl = localStorage.getItem('serverUrl');
+    var authToken = localStorage.getItem('authToken');
     if (typeof serverUrl !== 'string' || serverUrl.length === 0) {
       // handle invalid server url
     } else {
-      fetch(serverUrl, {
+      // curl -i -H "Accept: application/json"
+      //      -H "content-type: application/json"
+      //      -H "Authorization: a4PLf8QICdDdOmFjwdtSYqkCqn9CvN0VQt7mpUUf"
+      //      --data "@event.json" -X POST http://server/events
+      //
+      fetch(serverUrl + '/events', {
         method : 'POST',
-        body   : data,
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Authorization": authToken
+        },
+        body   : JSON.stringify({
+          "Event": {
+              "info": "test",
+              "Attribute": [
+                {
+                  'Category': 'Payload delivery',
+                  'type': 'email-body',
+                  'value': data
+                }
+              ]
+}
+        })
       }).then(function(response) {
-        console.log(response.status);
+        var eventId = response.Event.id;
+        console.log("Created event", eventId);
       }).catch(function(error) {
         console.log(error);
       });
