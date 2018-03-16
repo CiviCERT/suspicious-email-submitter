0fcab659aabbc20cd45af73604881f075b464884
lilia <liliakai@gmail.com>
2018-03-16 14:41:49 +0100

    Add todo, delete extra whitespace

--- bundle.js
+++ bundle.js
@@ -9,12 +9,11 @@ var rawDownloadUrl;
 if (url.host === "mail.google.com") {
   var selection = $('*[data-message-id]');
   if (selection.length > 0) {
+    //TODO: Handle multiple message ids on the page
     var messageId = selection[0].dataset['messageId'];
     // https://mail.google.com/mail/u/0?view=att&th=MESSAGEID&attid=0&disp=comp&safe=1&zw
     rawDownloadUrl = url.origin + url.pathname + "?view=att&th=" + messageId + "&attid=0&disp=comp&safe=1&zw";
-
   }
-
 } else if (url.host === "mail.yahoo.com") {
   // https://apis.mail.yahoo.com//ws/v3/mailboxes/@.id==VjN-R91iv4Ew7ABG6ZfcF9Y7tBeuWCmcCXV_rGL_DRMea7aKbRuuMjM9T2pzRtznS3DwBmBxuc2UTjmHlmQVvjcwMQ/messages/@.id==AOyS3goAAAMLWnG7EQJCIEz14Ds/content/rawplaintext?appid=YahooMailNeo&wssid=N54i/xPQPc9&ymreqid=a4b713d2-f6e2-6077-0111-1801db010000
   var mailboxId = ''; // fixme
