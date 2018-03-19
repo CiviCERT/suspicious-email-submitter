b0ddb64c2e910631029bec347a41469aa71744e8
lilia <liliakai@gmail.com>
2018-03-19 17:43:11 +0100

    Nest email Attributes within an Object
    A potentially more useful format for storing email data on a MISP server.

--- popup.js
+++ popup.js
@@ -33,18 +33,28 @@ if (data) {
         },
         body   : JSON.stringify({
           "Event": {
-              "info": "test",
-              "distribution": 0,
-              "threat_level_id": 3,
-              "analysis": 1,
-              "Attribute": [
-                {
-                  'Category': 'Payload delivery',
-                  'type': 'email-body',
-                  'value': data
-                }
-              ]
-}
+            "info": "test", // TODO: insert email subject here?
+            "distribution": 0,
+            "threat_level_id": 3,
+            "analysis": 1,
+            "Object": [
+              {
+                //TODO: Pull the following magic numbers from email template?
+                'name': 'email',
+                'meta-category': 'network',
+                'description': 'Email object describing an email with meta-information',
+                'template_uuid': 'a0c666e0-fc65-4be8-b48f-3423d788b552',
+                'template_version': 8,
+                'Attribute': [
+                  {
+                    'Category': 'Payload delivery',
+                    'type': 'email-body',
+                    'value': data
+                  }
+                ],
+              }
+            ]
+          }
         })
       }).then(function(response) {
         return response.json();
