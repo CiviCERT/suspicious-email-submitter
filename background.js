68dd00fac642ba8cc3270a55bde8d75a3dccab88
lilia <liliakai@gmail.com>
2018-03-20 18:29:38 +0100

    Apply the configured logo to the browser action

---
+++ background.js
@@ -0,0 +1,17 @@
+function updateNameAndImage() {
+  var config = SESConfig.getSelectedConfiguration();
+  if (typeof config.logo === 'string') {
+    var myCanvas = document.createElement('canvas');
+    var ctx = myCanvas.getContext('2d');
+    var img = new Image;
+    img.onload = function(){
+        ctx.drawImage(img,0,0, 100,100); // Or at whatever offset you like
+        var imageData = ctx.getImageData(0, 0, 100, 100);
+        chrome.browserAction.setIcon({imageData: imageData});
+    };
+    img.src = config.logo;
+  }
+}
+
+updateNameAndImage();
+//TODO: update when config changes
