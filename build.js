19c0fe9c2a3cf6ca1b837108c3e7806b4270df90
lilia <liliakai@gmail.com>
2018-06-27 20:38:33 +0200

    Decompose build script into npm scripts
    This simplifies the build a bit to just use a single dist/ directory

--- build.js
+++ build.js
@@ -3,30 +3,7 @@ const { exec } = require('child_process');
 const VERSION = require('./manifest.json').version;
 
 (async () => {
-    const catcmd = 'cat jquery-3.3.1.min.js contentScript.js > bundle.js';
-    console.log("Concatenating content scripts");
-    await new Promise(function(resolve, reject) {
-      exec(catcmd, (err, stdout, stderr) => {
-
-        // the *entire* stdout and stderr (buffered)
-        if (stdout) {
-          console.log(stdout);
-        }
-        if (stderr) {
-          console.log(stderr);
-        }
-
-        if (err) {
-          console.log(err);
-          reject(err);
-        } else {
-          console.log("Concatenating done");
-          resolve();
-        }
-      });
-    });
-    const filename = 'ses-' + VERSION;
-    const unpacked = filename + '-unpacked'
+    const unpacked = 'dist';
     console.log("Copying files to", unpacked);
     await cpy([
       'LICENSE',
@@ -47,27 +24,5 @@ const VERSION = require('./manifest.json').version;
       'icon_*.png'
     ], unpacked, { parents: true });
     console.log('Files copied to', unpacked);
-
-    const zipcmd = "pwd && zip -r " + filename + ".zip " + "*";
-    console.log('Running', zipcmd);
-    await new Promise(function(resolve, reject) {
-      exec(zipcmd, {cwd: unpacked}, (err, stdout, stderr) => {
-        // the *entire* stdout and stderr (buffered)
-        if (stdout) {
-          console.log(stdout);
-        }
-        if (stderr) {
-          console.log(stderr);
-        }
-
-        if (err) {
-          console.log(err);
-          reject(err);
-        } else {
-          console.log('Zip file created:', unpacked + '/' + filename + '.zip');
-          resolve();
-        }
-      });
-    });
 
 })();
