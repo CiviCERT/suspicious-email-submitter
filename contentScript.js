770770004629d266ba295026431f9a1430866a2d
lilia <liliakai@gmail.com>
2018-06-27 15:18:28 +0200

    Content script refactor / Adding mail providers
    Some structural changes to make it more straightforward to add new providers.
    
    Each provider is served by a dedicated function called getRawEmailProviderName.
    Each of these functions should return a promise which either resovles to a
    string result containing the raw email or rejects with a numeric error status
    code.
    
    For now, the popup/frontend determines what the codes mean, though that may have
    to change if we add other providers.

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,44 @@
 (function() {
   console.log('SES contentScript');
 
-  // construct download urls
-  var url = new URL(location);
-  var rawDownloadUrl;
+  function getRawEmail() {
+    // Call the function for the provider based on the page url
+    // FIXME: how to make this work on gsuite w/ custom domain?
+    var url = new URL(location);
 
-  if (url.host === "mail.google.com") {
+    if (url.host === "mail.google.com") {
+      return getRawEmailGmail();
+    } else if (url.host === "mail.yahoo.com") {
+      return getRawEmailYahoo();
+    }
+  }
 
+  function getRawEmailGmail() {
     /* GMAIL */
+    var url = new URL(location);
+    // Find the message id in the DOM
     var selection = $('*[data-message-id]:visible');
     if (selection.length > 0) {
       var messageId = selection[0].dataset['legacyMessageId'] || selection[0].dataset['messageId'];
+
+      // Construct the url that will serve the raw message
       // https://mail.google.com/mail/u/0?view=att&th=MESSAGEID&attid=0&disp=comp&safe=1&zw
-      rawDownloadUrl = url.origin + url.pathname + "?view=att&th=" + messageId + "&attid=0&disp=comp&safe=1&zw";
+      var rawDownloadUrl = url.origin + url.pathname + "?view=att&th=" + messageId + "&attid=0&disp=comp&safe=1&zw";
+
+      // return a promise
+      return new Promise(function(resolve, reject) {
+        $.get(rawDownloadUrl).then(function(result) {
+          resolve(result);
+        }).catch(function() {
+          reject(deferred.status);
+        });
+      });
     }
+  }
 
-  } else if (url.host === "mail.yahoo.com") {
-
+  function getRawEmailYahoo() {
     /* YAHOO */
+
     // Find the mailbox, message, and wssid parameters in the DOM
     var mailboxId, messageId, wssid;
 
@@ -45,20 +66,30 @@
       rawDownloadUrl = 'https://apis.mail.yahoo.com//ws/v3/mailboxes' +
         '/@.id==' + mailboxId + '/messages/@.id==' + messageId +
         '/content/rawplaintext?wssid=' + wssid;
-    }
 
+      // return a promise
+      return new Promise(function(resolve, reject) {
+        $.get(rawDownloadUrl).then(function(result) {
+          resolve(result);
+        }).catch(function() {
+          reject(deferred.status);
+        });
+      });
+    }
   }
 
-  if (rawDownloadUrl) {
-    // fetch emails
-    var deferred = $.get(rawDownloadUrl);
-    deferred.then(function(result) {
+  // Get the raw email asynchronously and send a message with the result
+  var promise = getRawEmail();
+
+  if (promise) {
+    promise.then(function(result) {
       // send message to popup
-      chrome.runtime.sendMessage(null, { status: deferred.status, result: result });
-    }).catch(function(deferred) {
-      chrome.runtime.sendMessage(null, { status: deferred.status });
+      chrome.runtime.sendMessage(null, { result: result });
+    }).catch(function(status) {
+      chrome.runtime.sendMessage(null, { status: status });
     });
   } else {
       chrome.runtime.sendMessage(null, { status: -1 });
   }
+
 })();
